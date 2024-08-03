7b26482a859e1f599d455b5d955cb93ca738395e
Levis Chisira <87423593+Proffesuer@users.noreply.github.com>
2024-08-03 14:19:41 +0300

    added a complete portfolio with a complete picture

--- app/views/partials/index/index.php
+++ app/views/partials/index/index.php
@@ -5,7 +5,7 @@
         <div  class=" py-5">
             <div class="container-fluid">
                 <div class="row ">
-                    <div class="col-sm-4 comp-grid">
+                    <div class="col-sm-12 comp-grid">
                         <div class="">
                             <div class="site-wrap">
                                 <div class="site-navbar py-2">
@@ -63,7 +63,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="site-blocks-cover" style="background-image: url('images/hero_1.jpg');">
+                                    <div class="site-blocks-cover" style="background-image: url('assets/images/hero_1.jpg');">
                                         <div class="container">
                                             <div class="row">
                                                 <div class="col-lg-7 mx-auto order-lg-2 align-self-center">
@@ -127,12 +127,12 @@
                                                         <div class="row">
                                                             <div class="col-sm-6 col-lg-4 text-center item mb-4">
                                                                 <span class="tag">Sale</span>
-                                                                <a href="shop-single.html"> <img src="images/product_01.png" alt="Image"></a>
+                                                                <a href="shop-single.html"> <img src="assets/images/product_01.png" alt="Image"></a>
                                                                     <h3 class="text-dark"><a href="shop-single.html">Bioderma</a></h3>
                                                                     <p class="price"><del>95.00</del> &mdash; $55.00</p>
                                                                 </div>
                                                                 <div class="col-sm-6 col-lg-4 text-center item mb-4">
-                                                                    <a href="shop-single.html"> <img src="images/product_02.png" alt="Image"></a>
+                                                                    <a href="shop-single.html"> <img src="assets/images/product_02.png" alt="Image"></a>
                                                                         <h3 class="text-dark"><a href="shop-single.html">Chanca Piedra</a></h3>
                                                                         <p class="price">$70.00</p>
                                                                     </div>
@@ -142,18 +142,18 @@
                                                                             <p class="price">$120.00</p>
                                                                         </div>
                                                                         <div class="col-sm-6 col-lg-4 text-center item mb-4">
-                                                                            <a href="shop-single.html"> <img src="images/product_04.png" alt="Image"></a>
+                                                                            <a href="shop-single.html"> <img src="assets/images/product_04.png" alt="Image"></a>
                                                                                 <h3 class="text-dark"><a href="shop-single.html">Cetyl Pure</a></h3>
                                                                                 <p class="price"><del>45.00</del> &mdash; $20.00</p>
                                                                             </div>
                                                                             <div class="col-sm-6 col-lg-4 text-center item mb-4">
-                                                                                <a href="shop-single.html"> <img src="images/product_05.png" alt="Image"></a>
+                                                                                <a href="shop-single.html"> <img src="assets/images/product_05.png" alt="Image"></a>
                                                                                     <h3 class="text-dark"><a href="shop-single.html">CLA Core</a></h3>
                                                                                     <p class="price">$38.00</p>
                                                                                 </div>
                                                                                 <div class="col-sm-6 col-lg-4 text-center item mb-4">
                                                                                     <span class="tag">Sale</span>
-                                                                                    <a href="shop-single.html"> <img src="images/product_06.png" alt="Image"></a>
+                                                                                    <a href="shop-single.html"> <img src="assets/images/product_06.png" alt="Image"></a>
                                                                                         <h3 class="text-dark"><a href="shop-single.html">Poo Pourri</a></h3>
                                                                                         <p class="price"><del>$89</del> &mdash; $38.00</p>
                                                                                     </div>
@@ -176,22 +176,22 @@
                                                                                     <div class="col-md-12 block-3 products-wrap">
                                                                                         <div class="nonloop-block-3 owl-carousel">
                                                                                             <div class="text-center item mb-4">
-                                                                                                <a href="shop-single.html"> <img src="images/product_03.png" alt="Image"></a>
+                                                                                                <a href="shop-single.html"> <img src="assets/images/product_03.png" alt="Image"></a>
                                                                                                     <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
                                                                                                     <p class="price">$120.00</p>
                                                                                                 </div>
                                                                                                 <div class="text-center item mb-4">
-                                                                                                    <a href="shop-single.html"> <img src="images/product_01.png" alt="Image"></a>
+                                                                                                    <a href="shop-single.html"> <img src="assts/images/product_01.png" alt="Image"></a>
                                                                                                         <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
                                                                                                         <p class="price">$120.00</p>
                                                                                                     </div>
                                                                                                     <div class="text-center item mb-4">
-                                                                                                        <a href="shop-single.html"> <img src="images/product_02.png" alt="Image"></a>
+                                                                                                        <a href="shop-single.html"> <img src="assets/images/product_02.png" alt="Image"></a>
                                                                                                             <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
                                                                                                             <p class="price">$120.00</p>
                                                                                                         </div>
                                                                                                         <div class="text-center item mb-4">
-                                                                                                            <a href="shop-single.html"> <img src="images/product_04.png" alt="Image"></a>
+                                                                                                            <a href="shop-single.html"> <img src="assets/images/product_04.png" alt="Image"></a>
                                                                                                                 <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
                                                                                                                 <p class="price">$120.00</p>
                                                                                                             </div>
@@ -212,14 +212,14 @@
                                                                                                         <div class="nonloop-block-3 no-direction owl-carousel">
                                                                                                             <div class="testimony">
                                                                                                                 <blockquote>
-                                                                                                                    <img src="images/person_1.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                                                                                                                    <img src="assets/images/person_1.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
                                                                                                                         <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat unde.&rdquo;</p>
                                                                                                                     </blockquote>
                                                                                                                     <p>&mdash; Kelly Holmes</p>
                                                                                                                 </div>
                                                                                                                 <div class="testimony">
                                                                                                                     <blockquote>
-                                                                                                                        <img src="images/person_2.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                                                                                                                        <img src="assets/images/person_2.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
                                                                                                                             <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore
                                                                                                                                 obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat
                                                                                                                             unde.&rdquo;</p>
@@ -228,7 +228,7 @@
                                                                                                                     </div>
                                                                                                                     <div class="testimony">
                                                                                                                         <blockquote>
-                                                                                                                            <img src="images/person_3.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                                                                                                                            <img src="assets/images/person_3.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
                                                                                                                                 <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore
                                                                                                                                     obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat
                                                                                                                                 unde.&rdquo;</p>
@@ -237,7 +237,7 @@
                                                                                                                         </div>
                                                                                                                         <div class="testimony">
                                                                                                                             <blockquote>
-                                                                                                                                <img src="images/person_4.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                                                                                                                                <img src="assets/images/person_4.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
                                                                                                                                     <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore
                                                                                                                                         obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat
                                                                                                                                     unde.&rdquo;</p>
@@ -249,11 +249,11 @@
                                                                                                                 </div>
                                                                                                             </div>
                                                                                                         </div>
-                                                                                                        <div class="site-section bg-secondary bg-image" style="background-image: url('images/bg_2.jpg');">
+                                                                                                        <div class="site-section bg-secondary bg-image" style="background-image: url('assets/images/bg_2.jpg');">
                                                                                                             <div class="container">
                                                                                                                 <div class="row align-items-stretch">
                                                                                                                     <div class="col-lg-6 mb-5 mb-lg-0">
-                                                                                                                        <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_1.jpg');">
+                                                                                                                        <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('assets/images/bg_1.jpg');">
                                                                                                                             <div class="banner-1-inner align-self-center">
                                                                                                                                 <h2>Pharma Products</h2>
                                                                                                                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex ad minus rem odio voluptatem.
@@ -262,7 +262,7 @@
                                                                                                                         </a>
                                                                                                                     </div>
                                                                                                                     <div class="col-lg-6 mb-5 mb-lg-0">
-                                                                                                                        <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_2.jpg');">
+                                                                                                                        <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('assets/images/bg_2.jpg');">
                                                                                                                             <div class="banner-1-inner ml-auto  align-self-center">
                                                                                                                                 <h2>Rated by Experts</h2>
                                                                                                                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex ad minus rem odio voluptatem.
