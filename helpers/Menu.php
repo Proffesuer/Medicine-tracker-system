ccab106d53a4eeba07c15cb48cdb570677dec0a3
Levis Chisira <87423593+Proffesuer@users.noreply.github.com>
2024-08-03 13:06:27 +0300

    made a new project

---
+++ helpers/Menu.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'medicine', 
+			'label' => 'Medicine', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'prescriptions', 
+			'label' => 'Prescriptions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'recommendations', 
+			'label' => 'Recommendations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'reviews', 
+			'label' => 'Reviews', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'test', 
+			'label' => 'Test', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'medicine', 
+			'label' => 'Medicine', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'prescriptions', 
+			'label' => 'Prescriptions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'recommendations', 
+			'label' => 'Recommendations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'reviews', 
+			'label' => 'Reviews', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'test', 
+			'label' => 'Test', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+			public static $gender = array(
+		array(
+			"value" => "Male", 
+			"label" => "Male", 
+		),
+		array(
+			"value" => "Female", 
+			"label" => "Female", 
+		),);
+		
+}
