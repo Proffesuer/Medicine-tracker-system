dfb8f9f680f612bb0486a0a77e297e412507d676
Proffesuer <levischisira0@gmail.com>
2024-08-10 01:38:04 +0300

    added a new theme

--- app/views/partials/appheader.php
+++ app/views/partials/appheader.php
@@ -1,4 +1,4 @@
-<div id="topbar" class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+<div id="topbar" class="navbar navbar-expand-md fixed-top navbar-dark bg-primary">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?php print_link(HOME_PAGE) ?>">
             <img class="img-responsive" src="<?php print_link(SITE_LOGO); ?>" /> <?php echo SITE_NAME ?>
