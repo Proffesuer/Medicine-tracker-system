8aa1a8ac110f593638e17ed5da33028acb2792ad
Proffesuer <levischisira0@gmail.com>
2024-08-10 02:22:37 +0300

    created a menu with icons

--- app/views/partials/appheader.php
+++ app/views/partials/appheader.php
@@ -6,11 +6,12 @@
             <?php 
             if(user_login_status() == true ){ 
             ?>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+            <button type="button" class="navbar-toggler dropdown-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+            </button>
+            <button type="button" id="sidebarCollapse" class="btn btn-primary">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse navbar-responsive-collapse">
-                <?php Html :: render_menu(Menu :: $navbartopleft  , "navbar-nav mr-auto" ); ?>
+            <div class="navbar-collapse collapse navbar-responsive-collapse">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
@@ -40,3 +41,43 @@
                 ?>
             </div>
         </div>
+        <?php 
+        if(user_login_status() == true ){ 
+        ?>
+        <nav id="sidebar" class="navbar-dark bg-primary">
+            <ul class="nav navbar-nav w-100 flex-column align-self-start">
+                <li class="menu-profile text-center nav-item">
+                    <a class="avatar" href="<?php print_link('account') ?>">
+                        <?php 
+                        if(!empty(USER_PHOTO)){
+                        ?>
+                        <img class="img-fluid" src="<?php print_link(set_img_src(USER_PHOTO,260,200)); ?>" />
+                            <?php
+                            }
+                            else{
+                            ?>
+                            <span class="avatar-icon"><i class="fa fa-user"></i></span>
+                            <?php
+                            }
+                            ?>
+                        </a>
+                        <h5 class="user-name">Hi 
+                            <?php echo ucwords(USER_NAME); ?>
+                            <small class="text-muted"><?php echo ACL::$user_role; ?> </small>
+                        </h5>
+                        <div class="dropdown menu-dropdown">
+                            <button class="btn btn-primary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa fa-user"></i>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <a class="dropdown-item" href="<?php print_link('account') ?>"><i class="fa fa-user"></i> My Account</a>
+                                <a class="dropdown-item" href="<?php print_link('index/logout?csrf_token=' . Csrf::$token) ?>"><i class="fa fa-sign-out"></i> Logout</a>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <?php Html :: render_menu(Menu :: $navbarsideleft  , "nav navbar-nav w-100 flex-column align-self-start"  , "accordion"); ?>
+            </nav>
+            <?php 
+            } 
+            ?>
