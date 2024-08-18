d20634afa935691656d2a1f0300dd565e4bbc513
Proffesuer <levischisira0@gmail.com>
2024-08-19 02:06:14 +0300

    added more page linkings

---
+++ app/medicine.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medicine</title>
+</head>
+<body>
+    <br><br><br>
+    <h1>Hello i am the medicine page</h1>
+</body>
+</html>
